172c27f74d93da4d152345cb30b34dc4a8f514a4
Steve Kelly <kd2cca@gmail.com>
2014-04-09 11:11:51 -0400

    adjust quiver sizes

--- matlab/plot_results.m
+++ matlab/plot_results.m
@@ -522,7 +522,7 @@ alpha=2*display_factor*Lcar; % scale factor for moment
 for N=1:nnp
     if ( nsd == 3)
         if ( (f(1,N) ~= 0 ) | (f(2,N) ~= 0) | (f(3,N) ~= 0))
-            quiver3(xn(1,N),xn(2,N),xn(3,N),f(1,N),f(2,N),f(3,N),delta,'k');
+            quiver3(xn(1,N),xn(2,N),xn(3,N),f(1,N),f(2,N),f(3,N),delta,'k','LineWidth',2);
         end;
     end
     if ( nsd == 2)
@@ -553,13 +553,14 @@ end;
 % reactions %
 %%%%%%%%%%%%%
 function plot_reactions(type,Lcar,Rcomp,Idb,display_factor,xn,nnp,ndf,nsd)
-beta=display_factor*Lcar/max(max(abs(Rcomp))); % scale factor for reactions
+beta=3*display_factor*Lcar/max(max(abs(Rcomp))); % scale factor for reactions
 alpha=2*display_factor*Lcar; % scale factor for moment
 for N=1:nnp
     RN=zeros(ndf);
     if (nsd >1)
         if ((Idb(1,N) ~= 0) | (Idb(2,N) ~= 0))
-            quiver(xn(1,N),xn(2,N),Rcomp(1,N),Rcomp(2,N),beta,'k');
+            h = quiver(xn(1,N),xn(2,N),Rcomp(1,N),Rcomp(2,N),beta,'k', 'LineWidth', 2);
+            adjust_quiver_arrowhead_size(h, 0.5/beta);
         end;
         if (strcmp(type, 'beam') & (Idb(3,N) ~= 0))
             if (Rcomp(3,N) > eps)
@@ -571,7 +572,8 @@ for N=1:nnp
         end;
     else
         if (Idb(1,N) ~= 0)
-            quiver(xn(1,N),0,Rcomp(1,N),0,beta,'k');
+            h = quiver(xn(1,N),0,Rcomp(1,N),0,beta,'k', 'LineWidth', 2);
+            adjust_quiver_arrowhead_size(h, 0.5/beta);
         end;
     end;
 end
@@ -856,3 +858,114 @@ end
 %         end
 %         colorbar;
 %     end 
+
+function adjust_quiver_arrowhead_size(quivergroup_handle, scaling_factor)
+% Make quiver arrowheads bigger or smaller.
+%
+% adjust_quiver_arrowhead_size(quivergroup_handle, scaling_factor)
+%
+% Example:
+%   h = quiver(1:100, 1:100, randn(100, 100), randn(100, 100));
+%   adjust_quiver_arrowhead_size(h, 1.5);   % Makes all arrowheads 50% bigger.
+%
+% Inputs:
+%   quivergroup_handle      Handle returned by "quiver" command.
+%   scaling_factor          Factor by which to shrink/grow arrowheads.
+%
+% Output: none
+
+% Kevin J. Delaney
+% December 21, 2011
+% BMT Scientific Marine Services (www.scimar.com)
+
+if ~exist('quivergroup_handle', 'var')
+    help(mfilename);
+    return
+end
+
+if isempty(quivergroup_handle) || any(~ishandle(quivergroup_handle))
+    errordlg('Input "quivergroup_handle" is empty or contains invalid handles.', ...
+             mfilename);
+    return
+end
+
+if length(quivergroup_handle) > 1
+    errordlg('Expected "quivergroup_handle" to be a single handle.', mfilename);
+    return
+end
+
+if ~strcmpi(get(quivergroup_handle, 'Type'), 'hggroup')
+    errrodlg('Input "quivergroup_handle" is not of type "hggroup".', mfilename);
+    return
+end
+
+if ~exist('scaling_factor', 'var') || ...
+   isempty(scaling_factor) || ...
+   ~isnumeric(scaling_factor)
+    errordlg('Input "scaling_factor" is missing, empty or non-numeric.', ...
+             mfilename);
+    return
+end
+
+if length(scaling_factor) > 1
+    errordlg('Expected "scaling_factor" to be a scalar.', mfilename);
+    return
+end
+
+if scaling_factor <= 0
+    errordlg('"Scaling_factor" should be > 0.', mfilename);
+    return
+end
+
+line_handles = get(quivergroup_handle, 'Children');
+
+if isempty(line_handles) || (length(line_handles) < 3) || ...
+   ~ishandle(line_handles(2)) || ~strcmpi(get(line_handles(2), 'Type'), 'line')
+    errordlg('Unable to adjust arrowheads.', mfilename);
+    return
+end
+
+arrowhead_line = line_handles(2);
+
+XData = get(arrowhead_line, 'XData');
+YData = get(arrowhead_line, 'YData');
+
+if isempty(XData) || isempty(YData)
+    return
+end
+
+%   Break up XData, YData into triplets separated by NaNs.
+first_nan_index = find(~isnan(XData), 1, 'first');
+last_nan_index  = find(~isnan(XData), 1, 'last');
+
+for index = first_nan_index : 4 : last_nan_index
+    these_indices = index + (0:2);
+    
+    if these_indices(end) > length(XData)
+        break
+    end
+    
+    x_triplet = XData(these_indices);
+    y_triplet = YData(these_indices);
+    
+    if any(isnan(x_triplet)) || any(isnan(y_triplet))
+        continue
+    end
+    
+    %   First pair.
+    delta_x = diff(x_triplet(1:2));
+    delta_y = diff(y_triplet(1:2));
+    x_triplet(1) = x_triplet(2) - (delta_x * scaling_factor);
+    y_triplet(1) = y_triplet(2) - (delta_y * scaling_factor);
+        
+    %   Second pair.
+    delta_x = diff(x_triplet(2:3));
+    delta_y = diff(y_triplet(2:3));
+    x_triplet(3) = x_triplet(2) + (delta_x * scaling_factor);
+    y_triplet(3) = y_triplet(2) + (delta_y * scaling_factor);
+    
+    XData(these_indices) = x_triplet;
+    YData(these_indices) = y_triplet;
+end
+
+set(arrowhead_line, 'XData', XData, 'YData', YData);
